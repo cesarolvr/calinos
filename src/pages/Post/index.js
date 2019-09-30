7082d792876ed0797b29b66404f555e69c6fc768
Cesar <cesar2012oliveira@gmail.com>
2019-09-30 00:33:11 -0300

    better division of steps

--- src/pages/Post/index.js
+++ src/pages/Post/index.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import * as R from "ramda";
 
-import './Post.scss'
+import "./Post.scss";
 
 // Steps
-import Step0 from "./Step0";
-import Step1 from "./Step1";
-import Step2 from "./Step2";
-import Step3 from "./Step3";
-import Step4 from "./Step4";
+import Step0 from "./Steps/Step0";
+import Step1 from "./Steps/Step1";
+import Step2 from "./Steps/Step2";
+import Step3 from "./Steps/Step3";
+import Step4 from "./Steps/Step4";
+
+import getGeolocation from "../../utils/geolocation";
 
 const Post = () => {
   const [step, setStep] = useState(0);
+
+  // TODO: transferir essa model para outro canto
   const [formValue, setFormValue] = useState({
     animal: {
       name: "",
@@ -31,6 +35,20 @@ const Post = () => {
     photos: []
   });
 
+  // TODO: reescrever isso aqui
+  getGeolocation().then(({ lat, lng }) => {
+    setFormValue({
+      ...formValue,
+      local: {
+        ...formValue.local,
+        pin: {
+          lat,
+          lng
+        }
+      }
+    });
+  });
+
   const nextStep = () => {
     return setStep(step <= 4 ? step + 1 : 4);
   };
@@ -39,7 +57,6 @@ const Post = () => {
   };
 
   console.log(formValue);
-  
 
   return R.cond([
     [R.equals(0), R.always(<Step0 nextStep={nextStep} />)],
--- src/utils/geolocation.js
+++ src/utils/geolocation.js
@@ -3,8 +3,8 @@ const getGeolocation = () =>
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(res => {
         return resolve({
-          lat: parseFloat(res.coords.latitude.toFixed(4)),
-          lng: parseFloat(res.coords.longitude.toFixed(4))
+          lat: parseFloat(res.coords.latitude.toFixed(5)),
+          lng: parseFloat(res.coords.longitude.toFixed(5))
         });
       });
     } else {
