66ab1265121874ce9487d30750110e6d5f7bdead
Cesar <cesar2012oliveira@gmail.com>
2019-10-06 15:51:57 -0300

    upload of multiple images working

--- src/pages/Post/Steps/Step3.js
+++ src/pages/Post/Steps/Step3.js
@@ -8,7 +8,7 @@ import firebase from "firebase/app";
 
 const Step3 = ({ prevStep, nextStep, formValue, setFormValue }) => {
   const { photos = [] } = formValue;
-  const readURL = e => {
+  const readFile = e => {
     e.preventDefault();
     const input = e.target;
     const fileExists = input.files && input.files[0];
@@ -29,6 +29,44 @@ const Step3 = ({ prevStep, nextStep, formValue, setFormValue }) => {
       reader.readAsDataURL(input.files[0]);
     }
   };
+  const uploadFile = () => {
+    const user = getCurrentUser();
+    const db = firebase.firestore();
+    const storageRef = firebase.storage().ref();
+    const userId = firebase.auth().currentUser.uid;
+    let newPhoto = [];
+    if (photos.length === 0) return;
+    for (let i = 0; i < photos.length; i++) {
+      const fileName = `pic-${Math.floor(Math.random() * 1000000000)}`;
+      const file = photos[i].file;
+      storageRef
+        .child(`images/${userId}/${fileName}.jpg`)
+        .put(file)
+        .then(({ state, metadata }) => {
+          if (state !== "success") return;
+          newPhoto.push(metadata.fullPath);
+          if (i === photos.length - 1) {
+            getGeolocation().then(l => {
+              db.collection("posts")
+                .doc()
+                .set({
+                  ...formValue,
+                  local: {
+                    ...formValue.local,
+                    pin: {
+                      lat: l.lat,
+                      lng: l.lng
+                    }
+                  },
+                  authorId: user.uid,
+                  photos: [...newPhoto]
+                })
+                .then(nextStep);
+            });
+          }
+        });
+    }
+  };
   return (
     <div className="panel post">
       <h1>Adicione fotos do seu bichinho</h1>
@@ -36,7 +74,7 @@ const Step3 = ({ prevStep, nextStep, formValue, setFormValue }) => {
         type="file"
         name="animalImage"
         accept="image/*"
-        onChange={readURL}
+        onChange={readFile}
       />
       {photos.map(({ blob }, index) => {
         return <img src={blob} alt="" key={index} className="preview" />;
@@ -45,47 +83,7 @@ const Step3 = ({ prevStep, nextStep, formValue, setFormValue }) => {
       <button type="button" className="button -secondary" onClick={prevStep}>
         Voltar
       </button>
-      <button
-        className="button"
-        onClick={() => {
-          const user = getCurrentUser();
-          const db = firebase.firestore();
-          const storageRef = firebase.storage().ref();
-          const userId = firebase.auth().currentUser.uid;
-          const fileName = `pic-${Math.floor(Math.random() * 1000000000)}`;
-          let newPhoto = [];
-          console.log("Click ===>", photos);
-          if (photos.length === 0) return;
-          for (let i = 0; i < photos.length; i++) {
-            storageRef
-              .child(`images/${userId}/${fileName}.jpg`)
-              .put(photos[i].file)
-              .then(({ state, metadata }) => {
-                if (state !== "success") return;
-                newPhoto.push(metadata.fullPath);
-                if (i === photos.length - 1) {
-                  getGeolocation().then(l => {
-                    db.collection("posts")
-                      .doc()
-                      .set({
-                        ...formValue,
-                        local: {
-                          ...formValue.local,
-                          pin: {
-                            lat: l.lat,
-                            lng: l.lng
-                          }
-                        },
-                        authorId: user.uid,
-                        photos: [ ...newPhoto ]
-                      })
-                      .then(nextStep);
-                  });
-                }
-              });
-          }
-        }}
-      >
+      <button className="button" onClick={uploadFile}>
         Próximo
       </button>
     </div>
