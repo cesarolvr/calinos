6b28488102f0b5f3091a482187c2f4a96001d5c5
Cesar <cesar2012oliveira@gmail.com>
2019-10-06 18:54:10 -0300

    builded to test

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
-const NotFound = () => <div>404</div>;
+const NotFound = () => <div>404
+  <Link to="/home">Voltar para Home</Link>
+</div>;
 
 export default NotFound;
