27367faa3787d3f0b95e92b27352da97bb39c4c7
Cesar <cesar2012oliveira@gmail.com>
2019-10-08 02:18:30 -0300

    unused function removed

--- src/api/database/index.js
+++ src/api/database/index.js
@@ -20,6 +20,7 @@ const isUserInDatabase = () =>
 
 const getPosts = () =>
   new Promise((resolve, reject) => {
+    // TODO: transferir isso para um service onde todo mundo importa de lá
     const databaseInstance = firebase.firestore();
     databaseInstance
       .collection("posts")
@@ -31,23 +32,4 @@ const getPosts = () =>
       .catch(reject);
   });
 
-const getUsers = () =>
-  new Promise((resolve, reject) => {
-    const databaseInstance = firebase.firestore();
-    // const currentUser = firebase.auth().currentUser;
-
-    databaseInstance
-      .collection("users")
-      .get()
-      .then(res => {
-        if (res.empty) reject();
-        res.forEach(snapshot => {
-          console.log(snapshot);
-
-          // if (snapshot.data()) resolve(snapshot.data());
-        });
-      })
-      .catch(reject);
-  });
-
-export { isUserInDatabase, getUsers, getPosts };
+export { isUserInDatabase, getPosts };
