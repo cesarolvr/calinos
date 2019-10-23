312c919e94608e9cd0fe279fccdb8ae37d639218
Cesar <cesar2012oliveira@gmail.com>
2019-10-22 22:39:51 -0300

    improvements on panel

--- src/components/Map/MapPanel/index.js
+++ src/components/Map/MapPanel/index.js
@@ -6,11 +6,13 @@ import * as R from "ramda";
 
 import "./MapPanel.scss";
 
-const MapPanel = ({ animal = {}, photos = [] }) => {
+const MapPanel = ({ animal = {}, photos = [], local = {} }) => {
   const hasContent = !R.isEmpty(animal);
-  console.log(hasContent);
 
   const { breed = "", color = "", name = "", size = "" } = animal;
+  const { comment = "", reference = "", street = "" } = local;
+
+  console.log(local);
 
   useEffect(() => {
     if (hasContent) {
@@ -18,7 +20,8 @@ const MapPanel = ({ animal = {}, photos = [] }) => {
         gap: 0
       }).mount();
     }
-  }, []);
+  }, [animal]);
+
   return (
     <div
       className={classNames("map-panel", {
@@ -27,43 +30,51 @@ const MapPanel = ({ animal = {}, photos = [] }) => {
     >
       {hasContent ? (
         <>
-          <h1 className="title">{name}</h1>
-          <p className="info breed">
-            A raça do {name} é <strong>{breed}</strong>
-          </p>
-          <p className="info size">
-            {name} é um animal de <strong>{size.toLowerCase()}</strong> porte
-          </p>
-          <p className="info color">
-            {name} é <strong>{color.toLowerCase()}</strong>
-          </p>
-          <div className="slider">
-            <div data-glide-el="track" className="glide__track">
-              <ul className="glide__slides">
-                {photos.map((photo, index) => (
-                  <li className="glide__slide" key={index}>
-                    <img src={photo} className="image" />
-                  </li>
-                ))}
-              </ul>
-              <div className="glide__bullets" data-glide-el="controls[nav]">
-                {photos.map((item, index) => {
-                  return (
-                    <button
-                      key={index}
-                      className="glide__bullet"
-                      data-glide-dir={`=${index}`}
-                    ></button>
-                  );
-                })}
+          <div className="panel-content">
+            <h1 className="title">{name}</h1>
+            <p className="info breed">
+              A raça do {name} é um <strong>{breed}</strong>
+            </p>
+            <p className="info size">
+              {name} é um animal de <strong>{size.toLowerCase()}</strong> porte
+            </p>
+            <p className="info color">
+              {name} é <strong>{color.toLowerCase()}</strong>
+            </p>
+            <div className="slider">
+              <div data-glide-el="track" className="glide__track">
+                <ul className="glide__slides">
+                  {photos.map((photo, index) => (
+                    <li className="glide__slide" key={index}>
+                      <img src={photo} className="image" />
+                    </li>
+                  ))}
+                </ul>
+                <div className="glide__bullets" data-glide-el="controls[nav]">
+                  {photos.map((item, index) => {
+                    return (
+                      <button
+                        key={index}
+                        className="glide__bullet"
+                        data-glide-dir={`=${index}`}
+                      ></button>
+                    );
+                  })}
+                </div>
               </div>
             </div>
-          </div>
 
-          <p className="comment">
-            O Fred fugiu da casa de sua dona Fernanda ontem por voltar das
-            20h00, próximo à estação Santo Amaro do Metrô.
-          </p>
+            <p className="comment">{comment}</p>
+
+            <div className="block-content reference">
+              <h3 className="subtitle">O ponto de referência</h3>
+              <p className="paragraph">{reference}</p>
+            </div>
+            <div className="block-content street">
+              <h3 className="subtitle">Último contato</h3>
+              <p className="paragraph">{street}</p>
+            </div>
+          </div>
 
           <Link className="button help" to="/chat">
             Ajudar
