c21698e90f90217db3643d1ef21bf67f88b7365e
Cesar <cesar2012oliveira@gmail.com>
2019-12-11 21:14:13 -0300

    feed html and sliders implemented

--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,42 +1,80 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Glide from "@glidejs/glide";
 
 import userPhoto from "../../assets/images/woman-user-photo.png";
 import dogPhoto from "../../assets/images/dog-photo.png";
 
+import "./CardList.scss";
+
 const CardList = ({ type = 1 }) => {
+  useEffect(() => {
+    const sliders = document.querySelectorAll(".slider-post");
+    for (const [index] of sliders.entries()) {
+      new Glide(`.slider-post-${index}`, {
+        gap: 0
+      }).mount();
+    }
+  }, []);
   return (
     <ul className="card-list">
-      <li className="item">
-        {type === 1 && (
-          <div className="card-header">
-            <div className="holder">
-              <img src={userPhoto} alt="" />
-            </div>
-            <div className="holder">
-              <div className="name">Maria Fernanda</div>
-              <div className="description">
-                Perdeu o <strong>Fred</strong> em Santo Amaro
+      {[1, 2, 4, 5, 6].map((_, index) => (
+        <li className="item" key={index}>
+          {type === 1 && (
+            <div className="card-header">
+              <div className="holder">
+                <div className="profile">
+                  <img src={userPhoto} alt="" />
+                </div>
+              </div>
+              <div className="holder">
+                <div className="name">Maria Fernanda</div>
+                <div className="description">
+                  Perdeu o <strong>Fred</strong> em Santo Amaro
+                </div>
               </div>
             </div>
-          </div>
-        )}
-        <div className="card-body">
-          <div className="slider">
-            <img src={dogPhoto} alt="" />
-          </div>
-          {type === 1 && (
-            <div className="control">
-              <button className="button">Presentear Fred</button>
-              <button className="button">Perguntar sobre Fred</button>
-            </div>
           )}
-          {type === 2 && (
-            <div className="panel">
-              Fred
+          <div className="card-body">
+            <div className="slider">
+              <div className={`slider-post slider-post-${index}`}>
+                <div data-glide-el="track" className="glide__track">
+                  <ul className="glide__slides">
+                    {[dogPhoto, dogPhoto].map((photo, index) => {
+                      return (
+                        <li className="glide__slide" key={index}>
+                          <img
+                            src={photo}
+                            className="image"
+                            // onClick={toggleLightbox}
+                          />
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <div className="glide__bullets" data-glide-el="controls[nav]">
+                    {[dogPhoto, dogPhoto].map((item, index) => {
+                      return (
+                        <button
+                          key={index}
+                          className="glide__bullet"
+                          data-glide-dir={`=${index}`}
+                        ></button>
+                      );
+                    })}
+                  </div>
+                </div>
+              </div>
             </div>
-          )}
-        </div>
-      </li>
+            {type === 1 && (
+              <div className="control">
+                <button className="button -gift">Presentear Fred</button>
+                <button className="button -ask">Perguntar sobre Fred</button>
+              </div>
+            )}
+            {type === 2 && <div className="panel">Fred</div>}
+          </div>
+        </li>
+      ))}
     </ul>
   );
 };
--- src/components/PageTitle/index.js
+++ src/components/PageTitle/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import './PageTitle.scss'
+
 const PageTitle = ({ title = "" }) => {
 return <h1 className="page-title">{title}</h1>;
 };
