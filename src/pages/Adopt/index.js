18c0ff13fef107103454ffa99dbe4661b03354ae
Cesar <cesar2012oliveira@gmail.com>
2019-12-12 01:25:10 -0300

    filter implemented

--- src/pages/Adopt/index.js
+++ src/pages/Adopt/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import './Adopt.scss'
+import "./Adopt.scss";
 
 // Components
 import Bar from "../../components/Bar";
@@ -11,6 +11,32 @@ const Adopt = () => {
   return (
     <div className="adopt">
       <PageTitle title="Adoção e apadrinhamento de bichinhos" />
+      <div className="filter">
+        <div className="select-wrapper">
+          <select name="" id="" className="select">
+            <option value="" defaultValue>
+              Tipo
+            </option>
+            <option value="">Cachorros</option>
+            <option value="">Gatos</option>
+          </select>
+        </div>
+        <div className="select-wrapper">
+          <select name="" id="" className="select">
+            <option value="">Porte</option>
+            <option value="">Pequeno</option>
+            <option value="">Médio</option>
+            <option value="">Grande</option>
+          </select>
+        </div>
+        <div className="select-wrapper">
+          <select name="" id="" className="select">
+            <option value="">Sexo</option>
+            <option value="">Macho</option>
+            <option value="">Fêmea</option>
+          </select>
+        </div>
+      </div>
       <CardList2 />
       <Bar />
     </div>
