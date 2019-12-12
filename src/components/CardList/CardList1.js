48c52b3ffb7d9a61d48d38f3e328b19fe056dd2d
Cesar <cesar2012oliveira@gmail.com>
2019-12-12 18:43:36 -0300

    some labels added to feed markup

--- src/components/CardList/CardList1.js
+++ src/components/CardList/CardList1.js
@@ -19,60 +19,77 @@ const CardList1 = ({ data = [] }) => {
   }, []);
   return (
     <ul className="card-list">
-      {data.map((_, index) => (
-        <li className="item" key={index}>
-          <div className="card-header">
-            <div className="holder">
-              <div className="profile">
-                <img src={userPhoto} alt="" />
-              </div>
-            </div>
-            <div className="holder">
-              <div className="name">Maria Fernanda</div>
-              <div className="description">
-                <Icon type="environment" theme="filled" />
-                Fred sumiu em Santo Amaro
-              </div>
-            </div>
-          </div>
-          <div className="card-body">
-            <div className="slider">
-              <div className={`slider-post slider-post-${index}`}>
-                <div data-glide-el="track" className="glide__track">
-                  <ul className="glide__slides">
-                    {[dogPhoto, dogPhoto].map((photo, index) => {
-                      return (
-                        <li className="glide__slide" key={index}>
-                          <img
-                            src={photo}
-                            className="image"
-                            // onClick={toggleLightbox}
-                          />
-                        </li>
-                      );
-                    })}
-                  </ul>
-                  <div className="glide__bullets" data-glide-el="controls[nav]">
-                    {[dogPhoto, dogPhoto].map((item, index) => {
-                      return (
-                        <button
-                          key={index}
-                          className="glide__bullet"
-                          data-glide-dir={`=${index}`}
-                        ></button>
-                      );
-                    })}
+      {data.map(
+        (
+          { ownerName = "", photos = [], postType = "", animal = {}, ...props },
+          index
+        ) => {
+          console.log(props);
+
+          return (
+            <li className="item" key={index}>
+              <div className="card-header">
+                <div className="holder">
+                  <div className="profile">
+                    <img src={userPhoto} alt="" />
+                  </div>
+                </div>
+                <div className="holder">
+                  <div className="name">{ownerName}</div>
+                  <div className="description">
+                    <Icon type="environment" theme="filled" />
+                    {animal.name || ""} foi passear de tarde
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="control">
-              <button className="button -gift">Presentear Fred</button>
-              <button className="button -ask">Perguntar sobre</button>
-            </div>
-          </div>
-        </li>
-      ))}
+              <div className="card-body">
+                <div className="slider">
+                  <div className={`slider-post slider-post-${index}`}>
+                    <div data-glide-el="track" className="glide__track">
+                      <ul className="glide__slides">
+                        {[dogPhoto, dogPhoto].map((photo, index) => {
+                          return (
+                            <li className="glide__slide" key={index}>
+                              {photos.map((photo, index) => (
+                                <img
+                                  key={index}
+                                  src={photo}
+                                  className="image"
+                                  // onClick={toggleLightbox}
+                                />
+                              ))}
+                            </li>
+                          );
+                        })}
+                      </ul>
+                      <div
+                        className="glide__bullets"
+                        data-glide-el="controls[nav]"
+                      >
+                        {[dogPhoto, dogPhoto].map((item, index) => {
+                          return (
+                            <button
+                              key={index}
+                              className="glide__bullet"
+                              data-glide-dir={`=${index}`}
+                            ></button>
+                          );
+                        })}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="control">
+                  <button className="button -gift">
+                    Presentear {animal.name || ""}
+                  </button>
+                  <button className="button -ask">Perguntar sobre</button>
+                </div>
+              </div>
+            </li>
+          );
+        }
+      )}
     </ul>
   );
 };
--- src/mock/index.js
+++ src/mock/index.js
@@ -3,30 +3,30 @@ export const favorites = [
     animal: {
       breed: "American Bobtail pelo curto",
       color: "Bege",
-      gender: "F",
-      name: "Shhdhshh",
-      size: "Médio",
+      gender: "M",
+      name: "Théo",
+      size: "Pequeno",
       type: "cat"
     },
   
     authorId: "cU0HAEw3JSf7gk0BYLLNziALozX2",
     id: "513653370",
     local: {
-      comment: "Snhshsh",
+      comment: "Ele estava com uma coleira vermelha",
       contactPhone: "115455442",
       pin: {
         lat: -23.5979,
         lng: -46.68374
       },
   
-      reference: "Jdhshsh",
-      street: "Hshshsh"
+      reference: "Perto do Shopping",
+      street: "Rua Alvares Penteado"
     },
   
     ownerId: "cU0HAEw3JSf7gk0BYLLNziALozX2",
-    ownerName: "Cesar Oliveira Faber",
+    ownerName: "Cesar Faber",
     photos: [
-      "https://firebasestorage.googleapis.com/v0/b/animalia-e55c3.appspot.com/o/images%2FcU0HAEw3JSf7gk0BYLLNziALozX2%2Fpic-545500801.jpg?alt=media&token=f57bd828-1bca-4ce0-a975-25d1ff722ea2"
+      "https://firebasestorage.googleapis.com/v0/b/animalia-e55c3.appspot.com/o/images%2FcU0HAEw3JSf7gk0BYLLNziALozX2%2Fpic-443967069.jpg?alt=media&token=afd2244d-702b-4c4f-8fb2-fc79ee9f722f"
     ],
     postType: "lost"
   }
