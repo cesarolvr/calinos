b89b252271fd66e20ead0d9c302ba233a3944ebc
Cesar <cesar2012oliveira@gmail.com>
2019-12-13 13:05:47 -0300

    carousel working

--- src/components/CardList/CardList2.js
+++ src/components/CardList/CardList2.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Modal, Carousel } from "antd";
 import Glide from "@glidejs/glide";
 import { Icon } from "antd";
+import classNames from "classnames";
 
 import "./CardList.scss";
 import "./Modal.scss";
@@ -9,6 +10,8 @@ import "./Carousel.scss";
 
 const CardList2 = ({ data = [] }) => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [carouselRef, setCarouselRef] = useState(null);
+  const [currentSlide, setCurrentSlide] = useState(0);
   useEffect(() => {
     const sliders = document.querySelectorAll(".slider-post-two");
     for (const [index, item] of sliders.entries()) {
@@ -16,6 +19,9 @@ const CardList2 = ({ data = [] }) => {
         gap: 0
       }).mount();
     }
+
+    const carousel = React.createRef();
+    setCarouselRef(carousel);
   }, []);
   const showModal = () => {
     setModalVisible(true);
@@ -29,33 +35,56 @@ const CardList2 = ({ data = [] }) => {
     setModalVisible(false);
   };
 
-  const onChange = (a, b, c) => {
-    console.log(a, b, c);
+  const onChange = (from, to) => {
+    setCurrentSlide(to)
   };
   return (
     <>
       <Modal
-        title="Basic Modal"
-        wrapClassName="modal-wrapper"
+        wrapClassName={classNames(`modal-wrapper -current-${currentSlide}`)}
         visible={modalVisible}
         onOk={handleOk}
         onCancel={handleCancel}
         footer={null}
       >
-        <Carousel afterChange={onChange}>
+        <Carousel beforeChange={onChange}  ref={node => setCarouselRef(node)}>
           <div className="slide-item">
-            <h3>1</h3>
+            <h3>Adoção e apadrinhamento de bichinhos</h3>
+            <p>
+              Eba! Ficamos muito felizes em saber que você tem interesse em
+              adotar um bichinho! Mas, antes de começarmos, é importante deixar
+              alguns pontos bem claros :)
+            </p>
           </div>
           <div className="slide-item">
-            <h3>2</h3>
+            <h3>Um bichinho é um filho. E dará trabalho como um.</h3>
+            <p>
+              Junto com todo amor que você receberá do seu bichinho adotado,
+              virão também responsabilidades importantes. Cuidar de pet dá
+              trabalho, mas é super bem recompensado!
+            </p>
           </div>
           <div className="slide-item">
-            <h3>3</h3>
+            <h3>É necessário saber sobre guarda resonsável.</h3>
+            <p>
+              Cuidados veterinários, alimentação, passeios e brincadeiras fazem
+              parte de uma guarda responsável. Antes de adotar, é importante
+              levar em conta sua rotina e condições financeiras para garantir o
+              bem-estar do seu bichinho.
+            </p>
           </div>
           <div className="slide-item">
-            <h3>4</h3>
+            <h3>Saiba que adotar um bichinho não é uma brincadeira.</h3>
+            <p>
+              Adotar um bichinho é assumir uma responsabilidade durante toda a
+              vida dele. A média de vida de um pet é de aproximadamente 14 anos.
+              Então, tenha isso em mente e se planeje ao adotar!
+            </p>
           </div>
         </Carousel>
+        <button className="button-next" onClick={() => carouselRef.next()}>
+          Próximo
+        </button>
       </Modal>
       <ul className="card-list -two">
         {data.map(
